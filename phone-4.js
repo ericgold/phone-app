c93f3b6b67905823df3f76132ce09387edc99ef0
ericgold <egold05@gmail.com>
2016-11-09 18:46:13 -0800

    module wrapper
    added IIFE around phone app code
    renamed functions

--- phone-4.js
+++ phone-4.js
@@ -1,105 +1,110 @@
-"use strict";
+(function(exports){
+	"use strict";
 
-// APP SHOULD BUY PHONES UNTIL NOT ENOUGH MONEY IS LEFT TO BUY 
-// ANOTHER ONE AND STILL HAVE 'THRESHOLD' $ LEFT
-// THEN APP BUYS ACCESSORIES UNTIL SAME CONDITION
-// THEN APP OUTPUTS HOW MANY PHONES AND ACCESSORIES WERE BOUGHT
-// AND HOW MUCH MONEY REMAINS IN THE ACCOUNT
+	// APP SHOULD BUY PHONES UNTIL NOT ENOUGH MONEY IS LEFT TO BUY 
+	// ANOTHER ONE AND STILL HAVE 'THRESHOLD' $ LEFT
+	// THEN APP BUYS ACCESSORIES UNTIL SAME CONDITION
+	// THEN APP OUTPUTS HOW MANY PHONES AND ACCESSORIES WERE BOUGHT
+	// AND HOW MUCH MONEY REMAINS IN THE ACCOUNT
 
-var TAX = 0.09;
-var PRICE = 500;
-var ACCESSORY_PRICE = 60;
-var THRESHOLD = 100;
+	var TAX = 0.09;
+	var PRICE = 500;
+	var ACCESSORY_PRICE = 60;
+	var THRESHOLD = 100;
 
-var PHONE_TAX = calcTax(PRICE);
-var ACCESSORY_TAX = calcTax(ACCESSORY_PRICE);
+	var PHONE_TAX = calcTax(PRICE); //need this.calcTax(PRICE) here???
+	var ACCESSORY_TAX = calcTax(ACCESSORY_PRICE);
 
-var PHONE_TOTAL = PRICE + PHONE_TAX;
-var ACCESSORY_TOTAL = ACCESSORY_PRICE + ACCESSORY_TAX;
+	var PHONE_TOTAL = PRICE + PHONE_TAX;
+	var ACCESSORY_TOTAL = ACCESSORY_PRICE + ACCESSORY_TAX;
 
 
-// HELPER FUNCTIONS ***********************
-function calcTax(amount) {
-	return amount * TAX;
-}
-
-function formatPrice(number) {
-	return "$"+parseFloat(Math.round(number * 100) / 100).toFixed(2);
-}
-
-// PURCHASE FUNCTION **********************
-// increments count of items purchased 
-
-function countPurchase(number) {
-	var count = 0;
-
-	return function(number) {
-		count+=number;
-		return count;
+	// HELPER FUNCTIONS ***********************
+	exports.calcTax = function(amount) {
+		return amount * TAX;
 	}
 
-}
-
-var buyPhone = countPurchase();
-var buyAccessory = countPurchase();
-
-
-// BANKING FUNCTION ***********************
-// subtracts amount of purchase (plus tax) from bank balance
-
-function calcBank(number) {
-	var balance = 2500;
-	
-	return function(number) {
-		balance -= number;
-		return balance;
+	exports.formatPrice = function(number) {
+		return "$"+parseFloat(Math.round(number * 100) / 100).toFixed(2);
 	}
-}
 
-var deductBank = calcBank();
-var checkBank = calcBank();
+	// PURCHASE FUNCTION **********************
+	// increments count of items purchased 
+
+	exports.countPurchase = function(number) {
+		var count = 0;
+
+		return function(number) {
+			count+=number;
+			return count;
+		}
+
+	}
+
+	var buyPhone = countPurchase();
+	var buyAccessory = countPurchase();
 
 
-// OUTPUT FUNCTION ************************
+	// BANKING FUNCTION ***********************
+	// subtracts amount of purchase (plus tax) from bank balance
 
-function printOutput() {
-	
-	var phonesPurchased = buyPhone(0);
-	var phoneTotal = phonesPurchased * PHONE_TOTAL;
-	var accessoriesPurchased = buyAccessory(0);
-	var accessoryTotal = accessoriesPurchased * ACCESSORY_TOTAL;
-	var totalPurchaseAmount = phoneTotal + accessoryTotal;
-	var balance = deductBank(0);
+	exports.calcBank = function(number) {
+		var balance = 2500;
+		
+		return function(number) {
+			balance -= number;
+			return balance;
+		}
+	}
 
-	console.log("You bought " + phonesPurchased + " phones for " + formatPrice(phoneTotal) + " with tax");
-	console.log("You bought " + accessoriesPurchased + " accessories for " + formatPrice(accessoryTotal) + " with tax");
-	console.log("You spent " + formatPrice(totalPurchaseAmount));
-	console.log("You have " + formatPrice(balance) + " left");
-}
+	var deductBank = calcBank();
+	var checkBank = calcBank();
 
-// IIFE/LOOP ***********************************
 
-(function() {
-	var startingFunds = checkBank(0);
-	var toSpend = startingFunds - THRESHOLD;
+	// OUTPUT FUNCTION ************************
 
-	console.log("You started with " + formatPrice(startingFunds));
-	console.log("You're willing to spend " + formatPrice(toSpend));
+	exports.printOutput = function() {
+		
+		var phonesPurchased = buyPhone(0);
+		var phoneTotal = phonesPurchased * PHONE_TOTAL;
+		var accessoriesPurchased = buyAccessory(0);
+		var accessoryTotal = accessoriesPurchased * ACCESSORY_TOTAL;
+		var totalPurchaseAmount = phoneTotal + accessoryTotal;
+		var balance = deductBank(0);
 
-	while (toSpend > 0) {
-		if (toSpend > PHONE_TOTAL) {
-			buyPhone(1);
-			deductBank(PHONE_TOTAL);
-			toSpend -= PHONE_TOTAL;
-			
-		} else if (toSpend > ACCESSORY_TOTAL) {
-			buyAccessory(1);
-			deductBank(ACCESSORY_TOTAL);
-			toSpend -= ACCESSORY_TOTAL;
+		console.log("You bought " + phonesPurchased + " phones for " + formatPrice(phoneTotal) + " with tax");
+		console.log("You bought " + accessoriesPurchased + " accessories for " + formatPrice(accessoryTotal) + " with tax");
+		console.log("You spent " + formatPrice(totalPurchaseAmount));
+		console.log("You have " + formatPrice(balance) + " left");
+	}
 
-		} else {
-			printOutput();
-			break;
-		} 	
-	} 
-})();
+	// IIFE/LOOP ***********************************
+
+	exports.startSpree = function() {
+		var startingFunds = checkBank(0);
+		var toSpend = startingFunds - THRESHOLD;
+
+		console.log("You started with " + formatPrice(startingFunds));
+		console.log("You're willing to spend " + formatPrice(toSpend));
+
+		while (toSpend > 0) {
+			if (toSpend > PHONE_TOTAL) {
+				buyPhone(1);
+				deductBank(PHONE_TOTAL);
+				toSpend -= PHONE_TOTAL;
+				
+			} else if (toSpend > ACCESSORY_TOTAL) {
+				buyAccessory(1);
+				deductBank(ACCESSORY_TOTAL);
+				toSpend -= ACCESSORY_TOTAL;
+
+			} else {
+				printOutput();
+				break;
+			} 	
+		} 
+	};
+
+})(this.phoneSpree = {});
+
+phoneSpree.startSpree();
